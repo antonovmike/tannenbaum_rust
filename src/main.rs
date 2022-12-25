c5e26292c178135be569e13ca1a07d4943b2f2b9
antonovmike <antonvma@outlook.com>
2022-12-25 16:58:03 +0500

    String based algorhythm

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,43 @@ use std::thread;
 use std::time::Duration;
 use colored::Colorize;
 
-fn main() { tannenbaum(7); }
+fn main() { tannenbaum(23); }
 
-fn tannenbaum(x: i32) {
+fn tannenbaum(x: u32) {
 	let mut top = 0;
 	let mut i = 1;
-
+	// let mut line = "".to_string();
+	
 	while top < x/2 {
 		for _empty_space in 0..2     { print!(" "); }
 		top += 1
 	}
 	if x%2 == 1 { print!(" ") }
-	println!("{}", "A".on_yellow());
+	println!("{}", " ".on_yellow());
 
 	while i <= x {
-		for _empty_space in 0..(x-i) { print!(" "); }
-		for _left_side   in 0..i     { print!("{}", "L".on_green()); }
-		for _middle      in i..i+1   { print!("{}", "M".on_blue()); }
-		for _right_side  in 0..i     { print!("{}", "R".on_magenta()); }
-		println!();
+		let mut line = "".to_string();
+		for _empty_space in 0..(x-i) { 
+			// print!(" ");
+			line.push(' ') 
+		}
+		for _left_side   in 0..i     { 
+			// print!("{}", " ".on_green()); 
+			line.push('L')
+		}
+		for _middle      in i..i+1   { 
+			// print!("{}", " ".on_blue()); 
+			line.push('M')
+		}
+		for _right_side  in 0..i     { 
+			// print!("{}", " ".on_magenta()); 
+			line.push('R')
+		}
+		println!("{line}");
 		thread::sleep(Duration::from_millis(120));
 		i += 1
 	}
 	
 	for _stem in 0..(x-1) { print!(" "); }
-	println!("{}", "ШШШ".on_red());
+	println!("{}", "   ".on_blue());
 }
