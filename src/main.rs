d44114125472a37f3b2a1364722011092ea92cb2
antonovmike <antonvma@outlook.com>
2022-12-26 09:23:46 +0500

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::thread;
 use std::time::Duration;
 
-fn main() { tannenbaum(7); }
+fn main() { tannenbaum(7) }
 
 fn tannenbaum(x: i32) {
 	let mut top = 0;
 	let mut i = 1;
 
 	while top < x/2 {
-		for _empty_space in 0..2     { print!(" "); }
+		for _empty_space in 0..2     { print!(" ") }
 		top += 1
 	}
-	if x%2 == 1 { print!(" ") }
+	
+	if x%2 == 1                      { print!(" ") }
+	
 	println!("A");
 
 	while i <= x {
@@ -19,11 +21,14 @@ fn tannenbaum(x: i32) {
 		for _left_side   in 0..i     { print!("L") }
 		for _middle      in i..i+1   { print!("M") }
 		for _right_side  in 0..i     { print!("R") }
+		
 		println!();
+		
 		thread::sleep(Duration::from_millis(120));
 		i += 1
 	}
 	
-	for _stem in 0..(x-1) { print!(" "); }
+	for _stem in 0..(x-1)            { print!(" ") }
+	
 	println!("ШШШ");
 }
